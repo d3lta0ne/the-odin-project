a90b81c19014b60fa6eb6743d0735c600176ba6c
David Bruno <davidbruno2000@gmail.com>
2019-05-27 09:17:25 -0400

    single round of rps finished

--- basic-projects/rps-game/rps.js
+++ basic-projects/rps-game/rps.js
@@ -1,3 +1,27 @@
 function computerPlay(){
-    
+    let options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random() * options.length)]; 
 }
+
+function playRound(playerSelection, computerSelection) {
+    //makes inputs case sensitive
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    //checks input boundaries
+    if( (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")
+        || (computerSelection != "rock" && computerSelection != "paper" && computerSelection != "scissors"))
+        return "WRONG INPUT";    
+
+    if(playerSelection == computerSelection){
+        return "it's a tie!";
+    }
+    else if( (playerSelection == "rock" && computerSelection == "scissors") 
+        ||  (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")){
+        return "player wins";
+    }
+    else{
+        return "computer wins";
+    }
+}
+console.log(playRound("rock", computerPlay()));
