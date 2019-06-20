3e0b7dccf141d59151ef9819cc37202ead3ece7f
David Bruno <davidbruno2000@gmail.com>
2019-06-20 01:32:25 -0400

    buttons added

--- basic-projects/etch-a-sketch-web/etch_script.js
+++ basic-projects/etch-a-sketch-web/etch_script.js
@@ -1,13 +1,22 @@
 const container = document.querySelector("#game-container");
-for(i = 0; i < 16*16; ++i){
+for(i = 0; i < 2*2; ++i){
         const block = document.createElement("div");
         block.classList.add("square");
         block.addEventListener("mouseenter", () => {
             block.classList.toggle("sketch");
           });
-        block.textContent = i;
+        // block.textContent = i;
         container.appendChild(block);   
 }
 
 
 
+var btn = document.querySelector('#reset');
+btn.addEventListener('click', () => {
+  alert("Hello--World");
+});
+
+btn = document.querySelector('#color-change');
+btn.addEventListener('click', () => {
+    alert("Hello--World");
+  });
