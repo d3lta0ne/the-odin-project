346d26479f2030c833c6002f3faa5505a76830f3
“David <davidzbruno@protonmail.com>
2020-07-24 03:22:20 -0400

    useEffect added

--- javascript-exercises/react/counter-button/index.js
+++ javascript-exercises/react/counter-button/index.js
@@ -1,48 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
 // Class Component
-class Counter extends React.Component {
-  state = {
-    counter: 0,
-  };
-  increment = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
 
-  componentDidMount() {
-    this.setState({counter: 42});
-  }
+// class Counter extends React.Component {
+//   state = {
+//     counter: 0,
+//   };
+//   increment = () => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
 
-  componentDidUpdate() {
-    alert("Number of clicks: " + this.state.counter);
-  }
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.counter}</p>
-        <button onClick={this.increment}>Increment</button>
-      </div>
-    );
-  }
-}
-
-// Functional Component using the useState Hook
-// function Counter() {
-//   const [counter, setCounter] = useState(0);
-
-//   function increment() {
-//     setCounter(counter + 1);
+//   componentDidMount() {
+//     this.setState({ counter: 42 });
 //   }
 
-//   return (
-//     <div>
-//       <p>{counter}</p>
-//       <button onClick={increment}>Increment</button>
-//     </div>
-//   );
+//   componentDidUpdate() {
+//     alert("Number of clicks: " + this.state.counter);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>{this.state.counter}</p>
+//         <button onClick={this.increment}>Increment</button>
+//       </div>
+//     );
+//   }
+// }
+
+// Functional Component
+
+function Counter() {
+  // useState
+  const [counter, setCounter] = useState(0);
+
+  // useEffect combines all the lifecycle methods into one
+  useEffect(() => {
+    alert("Number of clicks: " + counter);
+  });
+
+  /**
+   * By passing something into the second part of the useEffect method,
+   * it will only run when it is updated and not run on the initial mount.
+   */
+
+  // useEffect(() => {
+  //   // similar to "componentDidUpdate"
+  //   return () => {
+  //     // cleanup
+  //     // similar to "componentDidUnmount"
+  //   };
+  // }, [count]); // When dependencies are added the method will not run on the initial mount
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 }
 
 const el = <Counter />;
