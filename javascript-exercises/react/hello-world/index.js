bc8daa7d9bafbef6753b694fee3ec97a712a0713
“David <davidzbruno@protonmail.com>
2020-07-24 01:33:28 -0400

    shopping list -react

--- javascript-exercises/react/hello-world/index.js
+++ javascript-exercises/react/hello-world/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
+// Functional Component
 function Hello() {
   return <h1>Hello world.</h1>;
 }
+
+// Class Component
+// class Hello extends React.Component {
+//   render() {
+//     return <h1>Hello world.</h1>;
+//   }
+// }
 
 const el = <Hello />;
 ReactDOM.render(el, document.getElementById("root"));
