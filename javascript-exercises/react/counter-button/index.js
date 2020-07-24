5f7f39527958771622eb6e1ab63564b82b294db7
“David <davidzbruno@protonmail.com>
2020-07-24 03:04:04 -0400

    lifecycle method addded 'componentDidMount'

--- javascript-exercises/react/counter-button/index.js
+++ javascript-exercises/react/counter-button/index.js
@@ -3,37 +3,42 @@ import ReactDOM from "react-dom";
 import "./style.css";
 
 // Class Component
-// class Counter extends React.Component {
-//   state = {
-//     counter: 0,
-//   };
-//   increment = () => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
-//   render() {
-//     return (
-//       <div>
-//         <p>{this.state.counter}</p>
-//         <button onClick={this.increment}>Increment</button>
-//       </div>
-//     );
-//   }
-// }
+class Counter extends React.Component {
+  state = {
+    counter: 0,
+  };
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  componentDidMount() {
+    this.setState({counter: 42});
+  }
+  
+  render() {
+    return (
+      <div>
+        <p>{this.state.counter}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
 
 // Functional Component using the useState Hook
-function Counter() {
-  const [counter, setCounter] = useState(0);
+// function Counter() {
+//   const [counter, setCounter] = useState(0);
 
-  function increment() {
-    setCounter(counter + 1);
-  }
+//   function increment() {
+//     setCounter(counter + 1);
+//   }
 
-  return (
-    <div>
-      <p>{counter}</p>
-      <button onClick={increment}>Increment</button>
-    </div>
-  );
+//   return (
+//     <div>
+//       <p>{counter}</p>
+//       <button onClick={increment}>Increment</button>
+//     </div>
+//   );
 }
 
 const el = <Counter />;
