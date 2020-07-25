62e6dd2ee4b8f2239545d8730611f0b6185ff898
“David <davidzbruno@protonmail.com>
2020-07-25 02:05:40 -0400

    redux example

---
+++ javascript-exercises/redux/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./style.css";
+
+const store = createStore(reducer);
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  function incrementCounter(num) {
+    return {
+      type: "INCREMENT",
+      num: num,
+    };
+  }
+  const initialState = {
+    count: 0,
+  };
+
+  function reducer(state = initialState, action) {
+    switch (action.type) {
+      case "INCREMENT":
+        return { count: state.count + action.num };
+      default:
+        return state;
+    }
+  }
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+const el = (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+ReactDOM.render(el, document.getElementById("root"));
